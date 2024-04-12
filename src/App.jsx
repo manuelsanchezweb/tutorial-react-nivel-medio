2b959f8b6a70896b870cd04179976d7845e7e6bb
manuelsanchez2 <manusansan22@gmail.com>
2024-04-12 18:04:06 +0200

    Add react query for better todo handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { API_URL, NUMBER_OF_TODOS } from './config'
+import { useQuery } from 'react-query'
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const fetchTodos = async () => {
+  const response = await fetch(API_URL)
+  const data = await response.json()
+  data.splice(NUMBER_OF_TODOS)
+  return data
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data: todos, isLoading, error } = useQuery('todos', fetchTodos)
 
   return (
-    <>
-      <h1 className="font-bold text-3xl underline">Hola chavaless</h1>
-    </>
+    <main className="flex flex-col items-center justify-center min-h-screen px-8 gap-12">
+      <h1 className="font-bold text-3xl">Tutorial React Nivel Medio</h1>
+
+      {error && <h2 className="text-2xl">Error: {error.message}</h2>}
+      {isLoading && (
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5 w-full">
+          {Array.from({ length: NUMBER_OF_TODOS }).map((_, index) => (
+            <div
+              key={index}
+              className="animate-pulse bg-gray-200 h-[30px] w-full rounded-md"
+            ></div>
+          ))}
+        </div>
+      )}
+      {todos && (
+        <ul className="grid sm:grid-cols-2 md:grid-cols-3 gap-5">
+          {todos.map((todo) => (
+            <li
+              className="flex flex-col border border-1 border-black py-2 px-3"
+              key={todo.id}
+            >
+              {todo.title}
+            </li>
+          ))}
+        </ul>
+      )}
+    </main>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryProvider = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryProvider}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 )
